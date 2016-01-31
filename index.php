d87684867ef8a300bc4f13b917d5f443e3a02a88
Victoria Hooker <vhooker@cnm.edu>
2016-01-30 17:14:54 -0700

    documentation has three references. Committing now so I have a point of reference while I play with it for a bit.

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
+		<title>Bootstrap 101 Template</title>
+
+		<!--Bootstrap-->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
 		<!-- Bootstrap Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
 
 		<!-- Optional Bootstrap theme -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
 
-		<!-- Link to your customer CSS files here -->
+		<!-- Link to your custom CSS files here -->
 		<link type="text/css" href="css/style.css" rel="stylesheet"/>
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -26,7 +31,7 @@
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 
-		<title>hello world</title>
+
 	</head>
 	<body>
 		<p>hello world</p>
